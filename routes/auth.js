96ab6b9d186555371d1f9ecd87655234fb2b72d4
24044756 <163349600+24044756@users.noreply.github.com>
2026-06-05 02:56:00 +0200

    fix model paths

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
 const { body, validationResult } = require('express-validator');
-const User = require('../models/User');
+const User = require('../models/user');
 
 // Generate JWT Token
 const generateToken = (id) => {
